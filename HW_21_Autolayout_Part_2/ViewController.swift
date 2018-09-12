c89103f8a695110a798a7213aadfddf1b11a1729
Dynnikov Oleg <dinnikovoleg@gmail.com>
2018-09-13 02:35:13 +0300

    Initial Autolayout project part 2.

---
+++ HW_21_Autolayout_Part_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW_21_Autolayout_Part_2
+//
+//  Created by Oleg Dynnikov on 9/13/18.
+//  Copyright © 2018 Oleg Dynnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
